f2f81939656ba3ada36ac4228199324191c0a57b
Bigeard <robin.bigeard@gmail.com>
2022-04-01 15:19:30 +0200

    start exam

--- CakeMachine/Simulation/Algorithmes/Bigeard_Clermont.cs
+++ CakeMachine/Simulation/Algorithmes/Bigeard_Clermont.cs
@@ -1,4 +1,4 @@
-﻿using System.Runtime.CompilerServices;
+using System.Runtime.CompilerServices;
 using CakeMachine.Fabrication.ContexteProduction;
 using CakeMachine.Fabrication.Elements;
 using CakeMachine.Fabrication.Opérations;
@@ -6,49 +6,11 @@ using CakeMachine.Utils;
 
 namespace CakeMachine.Simulation.Algorithmes
 {
-    internal class DixParDix : Algorithme
+    internal class Bigeard_Clermont : Algorithme
     {
         /// <inheritdoc />
-        public override bool SupportsSync => true;
-
-        /// <inheritdoc />
         public override bool SupportsAsync => true;
-
-        /// <inheritdoc />
-        public override IEnumerable<GâteauEmballé> Produire(Usine usine, CancellationToken token)
-        {
-            var capacitéFour = usine.OrganisationUsine.ParamètresCuisson.NombrePlaces;
-
-            var postePréparation = usine.Préparateurs.Single();
-            var posteEmballage = usine.Emballeuses.Single();
-            var posteCuisson = usine.Fours.Single();
-
-            while (!token.IsCancellationRequested)
-            {
-                var plats = usine.StockInfiniPlats.Take(10);
-
-                var gâteauxCrus = plats.AsParallel().Select(postePréparation.Préparer);
-
-                var gâteauxCuits = CuireParLots(gâteauxCrus, posteCuisson, capacitéFour);
-                var gâteauxEmballés = gâteauxCuits.AsParallel().Select(posteEmballage.Emballer);
-
-                foreach (var gâteauEmballé in gâteauxEmballés)
-                    yield return gâteauEmballé;
-            }
-        }
-
-        private static IEnumerable<GâteauCuit> CuireParLots(IEnumerable<GâteauCru> gâteaux, Cuisson four, uint capacitéFour)
-        {
-            var queue = new Queue<GâteauCru>(gâteaux);
-
-            while (queue.Any())
-            {
-                var gâteauxCuits = four.Cuire(queue.Dequeue(capacitéFour).ToArray());
-                foreach (var gâteauCuit in gâteauxCuits)
-                    yield return gâteauCuit;
-            }
-        }
-
+        
         /// <inheritdoc />
         public override async IAsyncEnumerable<GâteauEmballé> ProduireAsync(
             Usine usine,
@@ -62,7 +24,7 @@ namespace CakeMachine.Simulation.Algorithmes
 
             while (!token.IsCancellationRequested)
             {
-                var plats = usine.StockInfiniPlats.Take(10);
+                var plats = usine.StockInfiniPlats.Take(30);
 
                 var gâteauxCrus = plats
                     .Select(postePréparation.PréparerAsync)
